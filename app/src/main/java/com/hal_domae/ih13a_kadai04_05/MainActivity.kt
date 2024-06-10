84a12d83719b5b0fafb927b880e66f302c8b3196
haldomae <hal.domae@gmail.com>
2024-06-10 11:00:36 +0900

    クイズを出題するメソッドを追加

--- app/src/main/java/com/hal_domae/ih13a_kadai04_05/MainActivity.kt
+++ app/src/main/java/com/hal_domae/ih13a_kadai04_05/MainActivity.kt
@@ -9,6 +9,14 @@ import com.hal_domae.ih13a_kadai04_05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var rightAnswer: String? = null // クイズの正解
+    private var quizCount = 1
+    private val quizData = mutableListOf(
+        listOf("パンはパンでも食べられないパンは", "フライパン"),
+        listOf("パンはパンでも食べられないパンは", "フライパン"),
+        listOf("パンはパンでも食べられないパンは", "フライパン")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +27,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        showNextQuiz()
+    }
+
+    // クイズを出題するメソッド
+    private fun showNextQuiz(){
+        // 問題番号を表示
+        // 問題番号はプレースホルダになっている
+        binding.countLabel.text = getString(R.string.count_label, quizCount)
+
+        val quiz = quizData[0]
+        binding.questionLabel.text = quiz[0]
     }
 }
